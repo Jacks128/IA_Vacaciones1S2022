b7810c5b207caf56faf70d55640ce1e7e5a7e2f2
Jackeline Benitez <jackelinebenitez112@gmail.com>
2022-06-07 20:17:30 -0600

    Tarea1 201709166

---
+++ script.js
@@ -0,0 +1,86 @@
+//Jackeline Benitez - 201709166
+
+const dirty_succesors=(n)=>{
+	switch (n) {
+		case 1:
+			return [3,5];
+		case 2:
+			return [4,6];
+		case 3:
+			return [7,1];
+		case 4:
+			return [8,2];
+		case 5:
+			return [6,1];
+		case 6:
+			return [2,5];
+		case 7:
+			return [8,3];
+		case 8:
+			return [4,7];
+		default:
+			return null;
+	}
+}
+
+const recorrerEstadosDesde =(nodo_inicio)=>{
+	let estados =[1,2,3,4,5,6,7,8];
+	let lista = [];
+	let current=nodo_inicio;
+	for (let index = 0; index < 8; index++) {
+		lista=[...lista,current];
+		estados=estados.filter(val=>val!==current);
+		if(estados.length>0){
+			const sucesores=dirty_succesors(current);
+			if(!lista.includes(sucesores[0])){
+				current=sucesores[0];
+			}else if(!lista.includes(sucesores[1])){
+				current=sucesores[1];
+			}else{
+				break;
+			}
+		}
+	}
+	return lista;
+}
+
+const appendState =(camino,i)=>{
+	if(i>7) return;
+	const value=camino[i];
+	switch (value) {
+		case 1:
+			document.getElementById("log").innerHTML+= `<br>Vacuum Location: A | A State: Dirty | B State: Dirty`;
+			break
+		case 2:
+			document.getElementById("log").innerHTML+= `<br>Vacuum Location: A | A State: Dirty | B State: Clean`;
+			break;
+		case 3:
+			document.getElementById("log").innerHTML+= `<br>Vacuum Location: A | A State: Clean | B State: Dirty`;
+			break;
+		case 4:
+			document.getElementById("log").innerHTML+= `<br>Vacuum Location: A | A State: Clean | B State: Clean`;
+			break;
+		case 5:
+			document.getElementById("log").innerHTML+= `<br>Vacuum Location: B | A State: Dirty | B State: Dirty`;
+			break;
+		case 6:
+			document.getElementById("log").innerHTML+= `<br>Vacuum Location: B | A State: Dirty | B State: Clean`;
+			break;
+		case 7:
+			document.getElementById("log").innerHTML+= `<br>Vacuum Location: B | A State: Clean | B State: Dirty`;
+			break;
+		case 8:
+			document.getElementById("log").innerHTML+= `<br>Vacuum Location: B | A State: Clean | B State: Clean`;
+			break;
+		default:
+			return;
+	}
+	setTimeout(() => {
+		appendState(camino,i+1);
+	}, 1000);
+}
+if (typeof window !== 'undefined') {
+const arr=recorrerEstadosDesde(1);
+document.getElementById("original").innerHTML+= JSON.stringify(arr);
+appendState(arr,0);
+}
